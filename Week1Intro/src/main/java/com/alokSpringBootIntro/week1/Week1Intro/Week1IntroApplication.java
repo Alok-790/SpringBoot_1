35aa85d7c460362c66d00e631543104816162479
Alok Shukla <1234alok90@gmail.com>
2025-01-14 14:52:57 +0530

    1.2 setting up

---
+++ Week1Intro/src/main/java/com/alokSpringBootIntro/week1/Week1Intro/Week1IntroApplication.java
@@ -0,0 +1,13 @@
+package com.alokSpringBootIntro.week1.Week1Intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1IntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week1IntroApplication.class, args);
+	}
+
+}
---
+++ Week1Intro/src/test/java/com/alokSpringBootIntro/week1/Week1Intro/Week1IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alokSpringBootIntro.week1.Week1Intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1IntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
